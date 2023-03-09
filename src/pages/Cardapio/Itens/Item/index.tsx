e9b7d1989253c2e6a6bf7cb3164911ec096f679b
Carlos Antônio <105462756+carlosaljuniorcg@users.noreply.github.com>
2023-03-09 11:09:38 -0300

    UpdateHoje

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,9 +2,11 @@ import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPrato';
 import { useNavigate } from 'react-router-dom';
+import { memo } from 'react';
 
-export default function Item(props: Prato) {
+function Item(props: Prato) {
   const { id, title, description, photo } = props;
+  id === 1 && console.log(title);
   const navigate = useNavigate();
   return (
     <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
@@ -21,3 +23,5 @@ export default function Item(props: Prato) {
     </div>
   );
 }
+
+export default memo(Item);
